d01f490ff6d99b4d7b52b9cc3c0e30bc7373d362
dheerajmehta <dheeraj_fw11_310@masai.school>
2022-04-13 11:02:36 +0530

    1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,13 +9,13 @@ export default function Navbar() {
   return (
     <>
     <div className = " -red-500 w-full h-48">
-     <div className = " -blue-500 w-full h-16 ">
+     <div className = "  border-blue-500 w-full h-16  ">
 		 
 	 <div className = "-black w-9/12 ml-36 mt-2 grid grid-cols-5  ">
-	 <div>
+	 <div className = " md:left-0">
 
 		
-<img className="   " src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="" />
+<img className=" mt-1  w-3/4 p-2  lg:ml-10 md:-ml-96 sm:ml-96 " src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="" />
  
  </div>
 
